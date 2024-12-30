e23905310b95e40985f42698c77077339e4503aa
Tulza <Tulza0410@gmail.com>
2024-12-30 16:37:48 +1300

    format generated for python

--- internal/gen/generate.go
+++ internal/gen/generate.go
@@ -30,20 +30,45 @@ func generateFile(problem models.Problem) (string, error) {
 
 func generateContent(problem models.Problem) []byte {
 
-	problemLink := problem.Link
-	if problemLink == "" {
-		problemLink = problem.QuestionLink
-	}
-
+	var title string
+	var difficulty string
 	var topics []string
+	var frontendId string
+	var problemLink string
+
+	// get title
+	title = problem.QuestionTitle
+
+	// get difficulty
+	difficulty = problem.Difficulty
+
+	// get topics
 	for _, topic := range problem.Topics {
 		topics = append(topics, topic.Slug)
 	}
 
+	// get frontend id
+	frontendId = problem.QuestionFrontendId
+
+	// get link
+	problemLink = problem.Link
+	if problemLink == "" {
+		problemLink = problem.QuestionLink
+	}
+
 	return []byte(
 		fmt.Sprintf(`
-#problem: %s
-#topics: %s
+# %s
+# %s | %s | #%s
+# %s 
 
-`, problemLink, strings.Join(topics, ", ")))
+class Solution:
+    pass
+`, 
+title,
+difficulty,
+strings.Join(topics, ", "),
+frontendId,
+problemLink, 
+))
 }
